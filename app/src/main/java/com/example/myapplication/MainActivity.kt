411b350a7a18b8b33bf75aadd18a3b4912f4188f
ri9ka <zshapcezuk@gmail.com>
2023-10-13 21:30:49 +0300

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,28 +4,48 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.VISIBLE
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     companion object{
         const val EMAIL_INFO: String = "email"
-        const val ACCESS_MESSAGE: String = "ACCESS_MESSAGE"
+        const val NAME_INFO : String = "name"
+        const val AGE_INFO : String = "age"
+
+
     }
 
     var startAcrivity:ActivityResultLauncher<Intent>? = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()){
        result ->
         val editText : EditText = findViewById(R.id.editTextTextEmailAddress)
+        val nameEditText : EditText = findViewById(R.id.editTextText)
+        val ageEditText :EditText = findViewById(R.id.editTextNumber)
         if (result.resultCode == RESULT_OK) {
 
+            val tv : TextView = findViewById(R.id.textView)
+            val tv2 : TextView = findViewById(R.id.textView3)
+            val tv3 : TextView = findViewById(R.id.textView4)
+
+            tv.visibility = VISIBLE
+            tv2.visibility = VISIBLE
+            tv3.visibility = VISIBLE
+
             val intent: Intent? = result.data
-            val email: String? = intent!!.getStringExtra(ACCESS_MESSAGE)
+            val email: String? = intent?.getStringExtra(EMAIL_INFO)
+            val name: String? = intent?.getStringExtra(NAME_INFO)
+            val age: String? = intent?.getStringExtra(AGE_INFO)
+
+
             editText.setText(email)
-        }
-        else {
+            nameEditText.setText(name)
+            ageEditText.setText(age)
+        } else {
             editText.setText("Ошибка доступа")
         }
 
@@ -33,14 +53,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     fun onButtonClick(view: View){
         var emailBox: EditText = findViewById(R.id.editTextTextEmailAddress)
+        val nameBox : EditText = findViewById(R.id.editTextText)
+        val ageBox : EditText = findViewById(R.id.editTextNumber)
+
+
+        val name : String = nameBox.text.toString()
         var email: String = emailBox.text.toString()
+        var age: String = ageBox.text.toString()
+
         var intent: Intent = Intent(this@MainActivity, MainActivity2::class.java)
-        intent.putExtra(EMAIL_INFO, email)
-        startAcrivity?.launch(intent)
+
+        if (email == "" || name == "" || age == ""){
+            Toast.makeText(this, "Заполните ваши данные!", Toast.LENGTH_SHORT).show()
+
+        }
+        else {
+            intent.putExtra(EMAIL_INFO, email)
+            intent.putExtra(NAME_INFO, name)
+            intent.putExtra(AGE_INFO, age)
+
+
+            startAcrivity?.launch(intent)
+        }
     }
 
 }
--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -3,7 +3,9 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +13,28 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
         val extras : Bundle? = intent.extras
         val em : EditText = findViewById(R.id.editTextTextEmailAddress)
+        val na : EditText = findViewById(R.id.editTextText2)
+        val ag : EditText = findViewById(R.id.editTextNumber2)
+
 
         if (extras != null){
             var email: String? = extras.getString(MainActivity.EMAIL_INFO)
+            val name : String? = extras.getString(MainActivity.NAME_INFO)
+            val age : String? = extras.getString(MainActivity.AGE_INFO)
+
             if (email!= null){
                 em.setText("$email")
+
+            }
+
+
+            if (name != null){
+                na.setText(name)
+
+            }
+
+            if (age != null){
+                ag.setText(age)
 
             }
          //   email_info.setText(intent.extras?.getString(MainActivity.EMAIL_INFO))
@@ -23,12 +42,37 @@ class MainActivity2 : AppCompatActivity() {
         }
     }
 
-    fun sendMessage(message: String){
-        var data: Intent = Intent()
-        data.putExtra(MainActivity.ACCESS_MESSAGE, message)
-        setResult(RESULT_OK, data)
+    fun sendMessage(email: String?, name: String?, age : String?){
+            val data = Intent()
 
-        finish()
+            data.putExtra(MainActivity.EMAIL_INFO, email)
+            data.putExtra(MainActivity.NAME_INFO, name)
+            data.putExtra(MainActivity.AGE_INFO, age)
+
+            setResult(RESULT_OK, data)
+
+            finish()
     }
+
+    fun btclickagain(view: View) {
+        val Email_BOX: EditText = findViewById(R.id.editTextTextEmailAddress)
+        val Name_BOX: EditText = findViewById(R.id.editTextText2)
+        val Age_BOX: EditText = findViewById(R.id.editTextNumber2)
+
+
+
+        val email: String? = Email_BOX.text.toString()
+        val name: String? = Name_BOX.text.toString()
+        val age: String? = Age_BOX.text.toString()
+        if (email == "" || name == "" || age == ""){
+            Toast.makeText(this, "Проверьте ваши данные и заполните их!", Toast.LENGTH_SHORT).show()
+
+        }
+        else {
+            sendMessage(email, name, age)
+        }
+
+    }
+
 
 }
